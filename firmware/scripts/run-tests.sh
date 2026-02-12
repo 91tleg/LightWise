564d4d17cf3c63bcabfb560310c066b21a9cee00
Isra Sookun <isra.sookun@bellevuecollege.edu>
2026-02-12 15:29:24 -0800

    Fix firmware scripts

--- firmware/scripts/run-tests.sh
+++ firmware/scripts/run-tests.sh
@@ -1,6 +1,8 @@
-#!usr/bin/env bash
-
-mkdir build -p
+#!/usr/bin/env bash
+set -euo pipefail
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR/../test"
+mkdir -p build
 cd build
 cmake ..
 make -j
