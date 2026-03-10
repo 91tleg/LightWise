590b09bba02947a2c41afef66bbb461ac284867c
91tleg <maxchou0417@gmail.com>
2026-03-09 22:24:05 -0700

    Change ambient sample time to 30 seconds

--- firmware/src/modules/ambient/ambient_task.cpp
+++ firmware/src/modules/ambient/ambient_task.cpp
@@ -11,7 +11,7 @@ namespace ambient
     {
         constexpr char kTag[] = "AmbeintTask";
         constexpr float kAlpha = 0.1f; /* Smoothing factor for EMA filter */
-        constexpr TickType_t kSamplePeriodMs = 60U * 1000U; /* 1 minute */
+        constexpr TickType_t kSamplePeriodMs = 30U * 1000U; /* 30 seconds */
     } /* anonymous namespace */
 
     void task( void * pvParameters )
@@ -31,31 +31,15 @@ namespace ambient
         for( ;; )
         {
             const bool updated = mgr.update( data );
-            LOGI( kTag, "Ambient update %d: %d.%02d",
-                  static_cast<int>( updated ),
-                  static_cast<int>( data.lux ),
-                  static_cast<int>( ( data.lux - static_cast<int>( data.lux ) ) * 100 ) );
+            LOGI( kTag, "Ambient update %s: %.2f lux",
+                  updated ? "success" : "failed", data.lux );
 
             const BaseType_t queueResult  = xQueueOverwrite( params->queue, &data );
-            if( queueResult == pdTRUE )
-            {
-                LOGD( kTag, "Ambient data queued successfully" );
-            }
-            else
-            {
-                LOGW( kTag, "Failed to queue ambient data" );
-            }
+            LOGI( kTag, "Data queue send %s", queueResult == pdTRUE ? : "success" : "failed" );
 
             const BaseType_t delayResult = xTaskDelayUntil( &lastWakeTime,
                                                             pdMS_TO_TICKS( kSamplePeriodMs ) );
-            if( delayResult == pdTRUE )
-            {
-                LOGD( kTag, "Task delayed successfully" );
-            }
-            else
-            {
-                LOGW( kTag, "Task delay failed" );
-            }
+            LOGI( kTag, "Task delay %s", delayResult == pdTRUE ? "success", "failed" );
         }
     }
 } /* namespace ambient */
