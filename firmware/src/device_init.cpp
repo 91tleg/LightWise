35e33e9aa9eef4eccbf3b657353f9385e2ca9fcf
91tleg <maxchou0417@gmail.com>
2026-03-12 01:20:20 -0700

    Add device initialization

---
+++ firmware/src/device_init.cpp
@@ -0,0 +1,67 @@
+#include "device_init.hpp"
+
+#include "hal_init.hpp"
+
+#include "utils/log.h"
+
+#include "hal/alspt19.h"
+#include "hal/c4001.h"
+#include "hal/dht11.h"
+#include "hal/led.h"
+#include "hal/lwnode.h"
+
+#define TAG "DeviceInit"
+
+namespace device
+{
+    namespace
+    {
+        constexpr char kTag[] = "DeviceInit";
+    } /* anonymous namespace */
+
+    constinit ambient::Alspt19 xAlsPt19Primary( &hal::xAlspt19Primary );
+    constinit ambient::Alspt19 xAlsPt19Secondary( &hal::xAlspt19Secondary );
+    constinit light::Led xLed( &hal::xLed );
+    constinit mmwave::C4001 xC4001Primary( &hal::xC4001Primary );
+    constinit mmwave::C4001 xC001Secondary( &hal::xC4001Secondary );
+    constinit th::Dht11 xDht11Primary( &hal::xDht11Primary );
+    constinit lorawan::Lwnode xLwnodePrimary( &hal::xLwnodePrimary );
+
+    void init()
+    {
+        if( !xAlsPt19Primary.init() )
+        {
+            LOGE( kTag, "Alspt19 primary init failed" );
+        }
+
+        if( !xAlsPt19Secondary.init() )
+        {
+            LOGE( kTag, "Alspt19 secondary init failed" );
+        }
+
+        if( !xC4001Primary.init() )
+        {
+            LOGE( kTag, "C4001 primary init failed" );
+        }
+
+        if( !xC001Secondary.init() )
+        {
+            LOGE( kTag, "C4001 secondary init failed" );
+        }
+
+        if( !xDht11Primary.init() )
+        {
+            LOGE( kTag, "Dht11 primary init failed" );
+        }
+
+        if( !xLed.init() )
+        {
+            LOGE( TAG, "Led init failed" );
+        }
+
+        if( !xLwnodePrimary.init() )
+        {
+            LOGE( TAG, "Lwnode init failed" );
+        }
+    }
+} /* namespace device */
---
+++ firmware/src/device_init.hpp
@@ -0,0 +1,23 @@
+#ifndef SRC_DEVICE_INIT_HPP
+#define SRC_DEVICE_INIT_HPP
+
+#include "lib/ambient/alspt19.hpp"
+#include "lib/mmwave/c4001.hpp"
+#include "lib/light/led.hpp"
+#include "lib/th/dht11.hpp"
+#include "lib/lorawan/lwnode.hpp"
+
+namespace device
+{
+    void init();
+
+    extern ambient::Alspt19 xAlsPt19Primary;
+    extern ambient::Alspt19 xAlsPt19Secondary;
+    extern light::Led xLed;
+    extern mmwave::C4001 xC4001Primary;
+    extern mmwave::C4001 xC001Secondary;
+    extern th::Dht11 xDht11Primary;
+    extern lorawan::Lwnode xLwnodePrimary;
+} /* namespace device */
+
+#endif /* SRC_DEVICE_INIT_HPP */
