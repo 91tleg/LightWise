1df751b2e8541791842f33e55d2cf800eb9218a7
91tleg <maxchou0417@gmail.com>
2026-04-18 12:22:11 -0700

    Update system initialization to use shared I2C

--- firmware/src/sys/hal_init.cpp
+++ firmware/src/sys/hal_init.cpp
@@ -1,6 +1,7 @@
 #include "hal_init.hpp"
 
 #include "hal/adc_units.h"
+#include "hal/i2c_bus.h"
 #include "board/board.hpp"
 #include "utils/log/log.h"
 
@@ -12,86 +13,92 @@ namespace hal
 
         constexpr char kTag[] { "HalInit" };
 
+        constinit AdcUnit sAdcUnit1 { .unit   = board::config.adcUnit1,
+                                      .handle = nullptr };
+
+        constinit I2cBus sI2cBus { .port   = board::config.i2cPort,
+                                   .scl    = board::config.i2cScl,
+                                   .sda    = board::config.i2cSda,
+                                   .handle = nullptr };
     }/* anonymous namespace */
 
-    static constinit AdcUnit xAdcUnit1 { .unit   = board::config.adcUnit1,
-                                         .handle = nullptr };
+    constinit AlsPt19Hw alspt19Primary { .channel = board::config.alsPt19PrimaryChannel,
+                                         .handle  = nullptr };
 
-    constinit AlsPt19Hw xAlspt19Primary { .channel = board::config.alsPt19PrimaryChannel,
-                                          .handle = nullptr };
+    constinit AlsPt19Hw alspt19Secondary { .channel = board::config.alsPt19SecondaryChannel,
+                                           .handle  = nullptr };
 
-    constinit AlsPt19Hw xAlspt19Secondary { .channel = board::config.alsPt19SecondaryChannel,
-                                            .handle = nullptr };
+    constinit C4001Hw c4001Primary { .uartNum   = board::config.c4001PrimaryUartNum,
+                                     .baud      = board::config.c4001PrimaryBaud,
+                                     .txPin     = board::config.c4001PrimaryTxPin,
+                                     .rxPin     = board::config.c4001PrimaryRxPin,
+                                     .rxBufSize = board::config.c4001PrimaryRxBufSize,
+                                     .txBufSize = board::config.c4001PrimaryTxBufSize };
 
-    constinit C4001Hw xC4001Primary { .uartNum = board::config.c4001PrimaryUartNum,
-                                      .baud = board::config.c4001PrimaryBaud,
-                                      .txPin = board::config.c4001PrimaryTxPin,
-                                      .rxPin = board::config.c4001PrimaryRxPin,
-                                      .rxBufSize = board::config.c4001PrimaryRxBufSize,
-                                      .txBufSize = board::config.c4001PrimaryTxBufSize };
+    constinit C4001Hw c4001Secondary { .uartNum   = board::config.c4001SecondaryUartNum,
+                                       .baud      = board::config.c4001SecondaryBaud,
+                                       .txPin     = board::config.c4001SecondaryTxPin,
+                                       .rxPin     = board::config.c4001SecondaryRxPin,
+                                       .rxBufSize = board::config.c4001SecondaryRxBufSize,
+                                       .txBufSize = board::config.c4001SecondaryTxBufSize };
 
-    constinit C4001Hw xC4001Secondary { .uartNum = board::config.c4001SecondaryUartNum,
-                                        .baud = board::config.c4001SecondaryBaud,
-                                        .txPin = board::config.c4001SecondaryTxPin,
-                                        .rxPin = board::config.c4001SecondaryRxPin,
-                                        .rxBufSize = board::config.c4001SecondaryRxBufSize,
-                                        .txBufSize = board::config.c4001SecondaryTxBufSize };
+    constinit Dht11Hw dht11Primary { .pin = board::config.dht11PrimaryPin };
 
-    constinit Dht11Hw xDht11Primary { .pin = board::config.dht11PrimaryPin };
+    constinit LedHw led { .pin               = board::config.ledPwmPin,
+                          .pwmChannel        = board::config.ledPwmChannel,
+                          .pwmTimer          = board::config.ledPwmTimer,
+                          .pwmFreqHz         = board::config.ledPwmFreqHz,
+                          .pwmResolutionBits = board::config.ledPwmDutyRes };
 
-    constinit LedHw xLed { .pin = board::config.ledPwmPin,
-                           .pwmChannel = board::config.ledPwmChannel,
-                           .pwmTimer = board::config.ledPwmTimer,
-                           .pwmFreqHz = board::config.ledPwmFreqHz,
-                           .pwmResolutionBits = board::config.ledPwmDutyRes };
-
-    constinit LwnodeHw xLwnodePrimary { .port = board::config.lwnodeI2cPort,
-                                        .sclPin = board::config.lwnodeI2cScl,
-                                        .sdaPin = board::config.lwnodeI2cSda,
-                                        .i2cAddr = board::config.lwnodeI2cAddr,
-                                        .busHandle = nullptr,
-                                        .devHandle = nullptr, };
+    constinit LwnodeHw lwnodePrimary { .bus    = &sI2cBus,
+                                       .addr   = board::config.lwnodeI2cAddr,
+                                       .handle = nullptr };
 
     void init()
     {
-        if( !adc_unit_init( &xAdcUnit1 ) )
+        if( !adc_unit_init( &sAdcUnit1 ) )
         {
             LOGE( kTag, "ADC unit 1 init failed" );
         }
 
-        if( !alspt19_hal_init( &xAlspt19Primary, xAdcUnit1.handle ) )
+        if( !alspt19_hal_init( &alspt19Primary, sAdcUnit1.handle ) )
         {
             LOGE( kTag, "AlsPt19 primary init failed" );
         }
 
-        if( !alspt19_hal_init( &xAlspt19Secondary, xAdcUnit1.handle ) )
+        if( !alspt19_hal_init( &alspt19Secondary, sAdcUnit1.handle ) )
         {
             LOGE( kTag, "AlsPt19 secondary init failed" );
         }
 
-        if( !c4001_hal_init( &xC4001Primary ) )
+        if( !c4001_hal_init( &c4001Primary ) )
         {
             LOGE( kTag, "C4001 primary init failed" );
         }
 
-        if( !c4001_hal_init( &xC4001Secondary ) )
+        if( !c4001_hal_init( &c4001Secondary ) )
         {
             LOGE( kTag, "C4001 secondary init failed" );
         }
 
-        if( !dht11_hal_init( &xDht11Primary ) )
+        if( !dht11_hal_init( &dht11Primary ) )
         {
             LOGE( kTag, "Dht11 primary init failed" );
         }
 
-        if( !led_hal_init( &xLed ) )
+        if( !led_hal_init( &led ) )
         {
             LOGE( kTag, "Led primary init failed" );
         }
 
-        if( !lwnode_hal_init( &xLwnodePrimary ) )
+        if( !i2c_bus_init( &sI2cBus ) )
         {
-            LOGW( kTag, "Lwnode primary init failed" );
+            LOGE( kTag, "I2c bus init failed" );
+        }
+
+        if( !lwnode_hal_init( &lwnodePrimary, &sI2cBus ) )
+        {
+            LOGE( kTag, "Lwnode primary init failed" );
         }
     }
 
