96c8ae90a066b1742f9880bb3c5a8a01b946332a
91tleg <maxchou0417@gmail.com>
2026-04-20 15:38:53 -0700

    Initialize struct members

--- firmware/src/common/types/sensor_health.hpp
+++ firmware/src/common/types/sensor_health.hpp
@@ -5,11 +5,11 @@
 
 enum class SensorHealth : uint8_t
 {
-    SYSTEM_OK = 0,
-    PRIMARY_FAIL,
-    SECONDARY_FAIL,
-    TOTAL_FAILURE,
-    DEGRADED,
+    SYSTEM_OK      = 0U,
+    PRIMARY_FAIL   = 1U,
+    SECONDARY_FAIL = 2U,
+    TOTAL_FAILURE  = 3U,
+    DEGRADED       = 4U,
 };
 
 #endif /* SRC_COMMON_TYPES_SENSOR_HEALTH_HPP */
