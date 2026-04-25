484226e45961e13f2b901bd7b216a5404be0a4b8
91tleg <maxchou0417@gmail.com>
2026-04-25 13:40:56 -0700

    Lazy initialzed DynamoDB resource

--- apps/api/src/infrastructure/persistence/dynamo/client.py
+++ apps/api/src/infrastructure/persistence/dynamo/client.py
@@ -2,12 +2,15 @@ import boto3
 from libs.config import settings
 
 
-_DYNAMODB = boto3.resource(
-    "dynamodb",
-    region_name=settings.AWS_REGION,
-    endpoint_url=settings.DYNAMO_ENDPOINT or None,
-)
+_DYNAMODB = None
 
 
 def get_dynamodb_resource():
+    global _DYNAMODB
+    if _DYNAMODB is None:
+        _DYNAMODB = boto3.resource(
+            "dynamodb",
+            region_name=settings.AWS_REGION,
+            endpoint_url=settings.DYNAMO_ENDPOINT or None,
+        )
     return _DYNAMODB
