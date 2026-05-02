769cbe9e9d07448f3db03b751e15f55af54544f4
91tleg <maxchou0417@gmail.com>
2026-05-01 20:35:10 -0700

    Add setGpioPolarity to test mock MMWave

--- firmware/test/mocks/lib/mock_mmwave_sensor.hpp
+++ firmware/test/mocks/lib/mock_mmwave_sensor.hpp
@@ -9,13 +9,14 @@ using namespace mmwave;
 class MockMmwaveSensor : public MmwaveSensor
 {
 public:
-    MOCK_METHOD( bool, connect,             (),                   ( noexcept, override ) );
-    MOCK_METHOD( bool, setSensorMode,       ( Mode ),             ( noexcept, override ) );
+    MOCK_METHOD( bool, connect,             (),                    ( noexcept, override ) );
+    MOCK_METHOD( bool, setSensorMode,       ( Mode ),              ( noexcept, override ) );
     MOCK_METHOD( bool, setDetectionRange,   ( uint16_t, uint16_t, uint16_t ), ( noexcept, override ) );
-    MOCK_METHOD( bool, setTrigSensitivity,  ( uint8_t ),          ( noexcept, override ) );
-    MOCK_METHOD( bool, setKeepSensitivity,  ( uint8_t ),          ( noexcept, override ) );
+    MOCK_METHOD( bool, setTrigSensitivity,  ( uint8_t ),           ( noexcept, override ) );
+    MOCK_METHOD( bool, setKeepSensitivity,  ( uint8_t ),           ( noexcept, override ) );
     MOCK_METHOD( bool, setDelay,            ( uint8_t, uint16_t ), ( noexcept, override ) );
-    MOCK_METHOD( bool, motionDetected,      ( bool & ),           ( noexcept, override ) );
+    MOCK_METHOD( bool, motionDetected,      ( bool & ),            ( noexcept, override ) );
+    MOCK_METHOD( bool, setGpioPolarity,     ( uint8_t ),           ( noexcept, override ) );
 };
 
 #endif /* TEST_MOCKS_LIB_MOCK_MMWAVE_SENSOR_HPP */
